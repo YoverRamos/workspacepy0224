ed1bffe4edeed7ae7af775a7360ffb6399b6afd3
YoverRamos <160413313+YoverRamos@users.noreply.github.com>
2024-02-18 17:24:16 +0000

    Ejercicio1

--- modulo1/practica/Ejercicio1.py
+++ modulo1/practica/Ejercicio1.py
@@ -1,11 +1,4 @@
-## determina el area de un circulo con radio pedido desde teclado
-
-import math # IMPORTAMOS LIBRERIA MATH
-
-PI =math.pi # float
-# uniendo declaracion , asignacion y parseado
-radio=float(input("ingrese el radio: "))
-
-area = PI*math.pow(radio,2)
-
-print(area)
+## Hola a todos buenas tarde, rellena tus datos
+Nombres=input("Ingrese tus Nombres: ")
+Apellidos=input("Ingrese tus Apellidos: ")
+DNI=input("Ingrese tu número de DNI: ")
